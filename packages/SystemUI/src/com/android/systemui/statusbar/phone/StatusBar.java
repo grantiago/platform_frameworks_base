e59f3cf2f6cef06f4aa1e9cfb145b59a41a723f0
SKULSHADY <anushekprasal@gmail.com>
2019-06-19 17:05:45 -0400

    SwitchStyler: Add support for theming system switches [1/3]
    Change-Id: If4982e19b25321fd8e7f6cfa30f5ccbfaf571c70
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4734,6 +4734,18 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
         ThemeAccentUtils.unlockQsTileStyles(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
     }
 
+    // Changes system switch to user selected style.
+    public void updateSwitchStyle() {
+         int switchStyle = Settings.System.getIntForUser(mContext.getContentResolver(),
+                 Settings.System.SWITCH_STYLER, 0, mLockscreenUserManager.getCurrentUserId());
+        ThemeAccentUtils.updateSwitchStyle(mOverlayManager, mLockscreenUserManager.getCurrentUserId(), switchStyle);
+    }
+
+     // Changes system switches back to stock.
+    public void stockSwitchStyle() {
+        ThemeAccentUtils.stockSwitchStyle(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
+    }
+
     private void updateDozingState() {
         Trace.traceCounter(Trace.TRACE_TAG_APP, "dozing", mDozing ? 1 : 0);
         Trace.beginSection("StatusBar#updateDozingState");
@@ -6047,6 +6059,9 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.LOCKSCREEN_DATE_SELECTION),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SWITCH_STYLER),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -6100,6 +6115,12 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
             } else if (uri.equals(Settings.System.getUriFor(
                 Settings.System.STATUS_BAR_TICKER_TICK_DURATION))) {
                 updateTickerTickDuration();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.SWITCH_STYLER))) {
+                mUiOffloadThread.submit(() -> {
+                    stockSwitchStyle();
+                    updateSwitchStyle();
+                });
             }
         }
 
